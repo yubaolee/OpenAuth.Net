61d5a26d7b614fea61daa48a3261c650224fc774
wintel <yubaolee@163.com>
2025-05-29 00:03:40 +0800

    docs: 增加移动端UniApp文档

--- newdocs/docs/.vuepress/navbar.ts
+++ newdocs/docs/.vuepress/navbar.ts
@@ -1,16 +1,17 @@
 /*
  * @Author: yubaolee <yubaolee@163.com> | ahfu~ <954478625@qq.com>
  * @Date: 2025-04-23 20:26:48
- * @LastEditTime: 2025-04-26 10:31:59
+ * @LastEditTime: 2025-05-28 23:58:52
  * @Description: 顶部超链接
  * Copyright (c) 2025 by yubaolee | ahfu~ , All Rights Reserved.  
  */
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: 'http://www.openauth.net.cn', icon: 'mdi:home' },
+  { text: '主页', link: 'http://www.openauth.net.cn', icon: 'mdi:home' },
   { text: '文档首页', link: '/', icon: 'mdi:book-open-page-variant' },
   { text: '开源版文档', link: '/notes/core/README.md', icon: 'logos:opensource' },
   { text: 'vue3版文档', link: '/notes/pro/README.md', icon: 'material-icon-theme:vue' },
-  { text: 'vue2版文档', link: '/notes/vue2/README.md', icon: 'material-icon-theme:vue' }
+  { text: 'vue2版文档', link: '/notes/vue2/README.md', icon: 'material-icon-theme:vue' },
+  { text: '移动UniApp文档', link: '/notes/mobile/README.md', icon: 'mdi:mobile' },
 ])
---
+++ newdocs/docs/notes/mobile/README.md
@@ -0,0 +1,31 @@
+---
+title: 移动UniApp版本介绍
+createTime: 2025/05/29 23:43:26
+permalink: /mobile/
+---
+
+OpenAuth.Net移动版基于uniapp开发，使用OpenAuth.Net的API接口（即：OpenAuth.WebApi）提供数据服务。
+
+
+
+
+## OpenAuth.Net系列教学视频
+
+[OpenAuth.Net视频合集--系统结构及代码下载](https://www.bilibili.com/video/BV1Z1421q7xU/)
+
+[OpenAuth.Net视频合集--企业版代码启动](https://www.bilibili.com/video/BV1KSuQebEek/)
+
+[OpenAuth.Net视频合集--使用企业版代码生成器](https://www.bilibili.com/video/BV1JCuyeaEFp/)
+
+[OpenAuth.Net视频合集--权限管理介绍](https://www.bilibili.com/video/BV1M9KeejENf/)
+
+[OpenAuth.Net视频合集--表单设计](https://www.bilibili.com/video/BV1dagEeFEVA/)
+
+[OpenAuth.Net视频合集--多表代码生成](https://www.bilibili.com/video/BV1r4J3zAEsx/)
+
+
+
+
+
+
+
